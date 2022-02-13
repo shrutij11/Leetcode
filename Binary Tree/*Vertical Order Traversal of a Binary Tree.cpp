e3a2c95e546281a83afe715d7ed65bc4dafe1f25
Shruti Jaiswal <81301875+shrutij11@users.noreply.github.com>
2022-02-13 12:58:02 +0530

    Create *Vertical Order Traversal of a Binary Tree.cpp

---
+++ Binary Tree/*Vertical Order Traversal of a Binary Tree.cpp
@@ -0,0 +1,33 @@
+// 987. Vertical Order Traversal of a Binary Tree
+// Hard
+
+
+// Given the root of a binary tree, calculate the vertical order traversal of the binary tree.
+// For each node at position (row, col), its left and right children will be at positions (row + 1, col - 1) and (row + 1, col + 1) respectively. The root of the tree is at (0, 0).
+// The vertical order traversal of a binary tree is a list of top-to-bottom orderings for each column index starting from the leftmost column and ending on the rightmost column. There may be multiple nodes in the same row and same column. In such a case, sort these nodes by their values.
+
+// Return the vertical order traversal of the binary tree.
+
+ vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,map<int,multiset<int>>>mp;
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        q.push({root,{0,0}});
+        while(q.size()){
+            auto p = q.front();
+            q.pop();
+            int x=p.second.first,y = p.second.second;
+            TreeNode* node =p.first;
+            mp[x][y].insert(node->val);
+            if(node->left)q.push({node->left,{x-1,y+1}});
+             if(node->right)q.push({node->right,{x+1,y+1}});
+        }
+        vector<vector<int>> ans;
+        for (auto p : mp) {
+            vector<int> col;
+            for (auto q : p.second) {
+                col.insert(col.end(), q.second.begin(), q.second.end());
+            }
+            ans.push_back(col);
+        }
+        return ans;
+    }

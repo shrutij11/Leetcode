1c4252b12b79fcf0f680af2778cb2a3be98c5b45
Shruti Jaiswal <81301875+shrutij11@users.noreply.github.com>
2022-04-14 17:35:53 +0530

    Create Find if Path Exist in Graph.cpp

---
+++ Graph/Find if Path Exist in Graph.cpp
@@ -0,0 +1,37 @@
+// There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+
+// You want to determine if there is a valid path that exists from vertex source to vertex destination.
+
+// Given edges and the integers n, source, and destination, return true if there is a valid path from source to destination, or false otherwise.
+
+
+
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
+          unordered_map<int,vector<int>> graph; 
+        for(auto e : edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+        vector<bool> visited(n,0);        
+        queue<int> q;
+        q.push(start);
+        visited[start] = 1; 
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            if(curr == end)
+                return 1; 
+            for(auto &node : graph[curr]){
+                if(!visited[node]){
+                    visited[node] = 1; 
+                    q.push(node);
+                }
+            }
+        }
+        
+        return false;
+    }
+};

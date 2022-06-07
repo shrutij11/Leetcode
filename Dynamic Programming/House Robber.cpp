ff34e3effcc137f802c285e7be54746b4e796b52
Shruti Jaiswal <81301875+shrutij11@users.noreply.github.com>
2022-06-07 18:04:41 +0530

    Update House Robber.cpp
    space optimization

--- Dynamic Programming/House Robber.cpp
+++ Dynamic Programming/House Robber.cpp
@@ -36,3 +36,21 @@ public:
         return f(nums,n-1,dp);
     }
 };
+//Space Optimization 
+int rob(vector<int>& nums) {
+      
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1; i<n; i++){
+            int take=nums[i];
+            if(i>1)take+=prev2;
+            int nottake=prev;
+            int curi= max(take,nottake);
+            prev2=prev;
+            prev=curi;
+        }
+        
+        return prev;
+    }
+};
